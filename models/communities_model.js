4a70987418e9e09ba17a78806f448524071fd009
zeel91297 <zeel91297@gmail.com>
2018-01-02 00:24:54 +0530

    community and post join

--- models/communities_model.js
+++ models/communities_model.js
@@ -1,22 +1,28 @@
-var db=require('../dbconnection');
+var db = require('../dbconnection');
 
-var community={
+var community = {
 
-    getAlCommunities:function(callback){
-        return db.query("select * from communities_tbl",callback);
+    getAlCommunities: function (callback) {
+        return db.query("select * from communities_tbl", callback);
     },
-    getCommunityById:function(id,callback){
-        return db.query("select * from communities_tbl where comm_id=?",[id],callback);
+    getCommunityById: function (id, callback) {
+        return db.query("select * from communities_tbl where comm_id=?", [id], callback);
     },
-    addCommunity:function(comm,callback){
-        return db.query("insert into communities_tbl values(?,?,?,?,?,?,?)",[null,comm.comm_name,comm.comm_des,comm.comm_pic,comm.comm_date,comm.comm_rating,comm.created_by],callback);
+    addCommunity: function (comm, callback) {
+        return db.query("insert into communities_tbl values(?,?,?,?,?,?,?)", [null, comm.comm_name, comm.comm_des, comm.comm_pic, comm.comm_date, comm.comm_rating, comm.created_by], callback);
     },
-    updateCommunity:function(id,comm,callback){
-        return db.query("update communities_tbl set comm_name=?,comm_des=?,comm_pic=?,comm_date=?,comm_rating=? where comm_id=?",[comm.comm_name,comm.comm_des,comm.comm_pic,comm.comm_date,comm.comm_rating,id],callback);
+    updateCommunity: function (id, comm, callback) {
+        return db.query("update communities_tbl set comm_name=?,comm_des=?,comm_pic=?,comm_date=?,comm_rating=? where comm_id=?", [comm.comm_name, comm.comm_des, comm.comm_pic, comm.comm_date, comm.comm_rating, id], callback);
     },
-    deleteCommunity:function(id,callback){
-        return db.query("delete from communities_tbl where comm_id=?",[id],callback);
+    deleteCommunity: function (id, callback) {
+        return db.query("delete from communities_tbl where comm_id=?", [id], callback);
+    },
+    getPostsByCommunityId: function (id, callback) {
+        return db.query("select c.*,p.*,u.* from communities_tbl c,post_tbl p,user_tbl u where c.comm_id=p.fk_comm_id and u.user_id=p.fk_user_id and c.comm_id=?", [id], callback);
+    },
+    getPostsByCommunity: function (callback) {
+        return db.query("select c.*,p.*,u.* from communities_tbl c,post_tbl p,user_tbl u where c.comm_id=p.fk_comm_id and u.user_id=p.fk_user_id", callback);
     }
 };
 
-module.exports=community;
+module.exports = community;
--- models/post_model.js
+++ models/post_model.js
@@ -1,22 +1,25 @@
-var db=require('../dbconnection');
+var db = require('../dbconnection');
 
-var post={
+var post = {
 
-    getAllPosts:function(callback){
-        return db.query("select * from post_tbl",callback);
+    getAllPosts: function (callback) {
+        return db.query("select * from post_tbl", callback);
     },
-    getPostById:function(id,callback){
-        return db.query("select * from post_tbl where post_id=?",[id],callback);
+    getPostById: function (id, callback) {
+        return db.query("select * from post_tbl where post_id=?", [id], callback);
     },
-    addPost:function(ps,callback){
-        return db.query("insert into post_tbl values(?,?,?,?,?,?)",[ps.post_title,ps.post_des,ps.post_pic,ps.post_date,ps.fk_user_id,ps.fk_comm_id],callback);
+    addPost: function (ps, callback) {
+        return db.query("insert into post_tbl values(?,?,?,?,?,?,?)", [null, ps.post_title, ps.post_des, ps.post_pic, ps.post_date, ps.fk_user_id, ps.fk_comm_id], callback);
     },
-    updatePost:function(id,ps,callback){
-        return db.query("update post_tbl set post_title=?,post_des=?,post_pic=?,post_date=?,fk_user_id=?,fk_comm_id=? where post_id=?",[ps.post_title,ps.post_des,ps.post_pic,ps.post_date,ps.fk_user_id,ps.fk_comm_id,id],callback);
+    updatePost: function (id, ps, callback) {
+        return db.query("update post_tbl set post_title=?,post_des=?,post_pic=?,post_date=? where post_id=?", [ps.post_title, ps.post_des, ps.post_pic, ps.post_date, id], callback);
     },
-    deletePost:function(id,callback){
-        return db.query("delete from post_tbl where post_id=?",[id],callback);
+    deletePost: function (id, callback) {
+        return db.query("delete from post_tbl where post_id=?", [id], callback);
+    },
+    getPostAndUser: function (id, callback) {
+        return db.query("select p.*,u.* from post_tbl p,user_tbl u where p.fk_user_id=u.user_id and p.post_id=?", [id],callback);
     }
 };
 
-module.exports=post;
+module.exports = post;
