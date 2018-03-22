1fafb1158b3efe9305e9f909f7b28e7bd77e6997
zeel91297 <zeel91297@gmail.com>
2018-03-23 01:49:30 +0530

    changes

--- routes/user_login.js
+++ routes/user_login.js
@@ -1,19 +1,18 @@
-var express=require('express');
-var router=express.Router();
-var u=require('../models/user_login_model');
+var express = require('express');
+var router = express.Router();
+var u = require('../models/user_login_model');
 
-router.post('/',function(req,res,net){
-    u.getUser(req.body,function(err,rows){
-      if(err)
-      {
-        res.json(err);
-        //alert('not valid');
-      }
-      else{
-        
-        //alert('valid');
-          res.json(rows);
-      }
-    });
+router.post('/', function (req, res, net) {
+  u.getUser(req.body, function (err, rows) {
+    console.log(req.body);
+    if (err) {
+      res.json(err);
+      //alert('not valid');
+    } else {
+
+      //alert('valid');
+      res.json(rows);
+    }
   });
-  module.exports = router;
+});
+module.exports = router;
