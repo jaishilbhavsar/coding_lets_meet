3192451d3ffc16fe6acd51b6613267ad95cc2ee9
zeel91297 <zeel91297@gmail.com>
2018-04-27 16:13:48 +0530

    user token changed

--- models/user_login_model.js
+++ models/user_login_model.js
@@ -1,10 +1,9 @@
 var db = require('../dbconnection');
-var u={
-getUser:function(us,callback)
-{
-    console.log(us.user_id);
-    console.log(us.user_pass);
-    return db.query("select * from user_tbl where user_id=? and user_pass=?", [us.user_id,us.user_pass], callback);
-}
+var u = {
+    getUser: function (us, callback) {
+        console.log(us.user_id);
+        console.log(us.user_pass);
+        return db.query("select * from user_tbl where user_id=? and user_pass=? and token=?", [us.user_id, us.user_pass, us.token], callback);
+    }
 };
-module.exports=u;
+module.exports = u;
