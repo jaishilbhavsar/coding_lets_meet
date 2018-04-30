fe7ae2fc00af561c22aa883dc139a8b73b1b6044
zeel91297 <zeel91297@gmail.com>
2018-04-30 18:24:14 +0530

    counts

--- models/all_count_model.js
+++ models/all_count_model.js
@@ -2,14 +2,13 @@ var db = require('../dbconnection');
 var fs = require('fs');
 var allcounts = {
     eventCount: function (callback) {
-        return db.query("select COUNT(*) from event_tbl", callback);
+        return db.query("select * from event_tbl", callback);
     },
     commuinityCount: function (callback) {
-        return db.query("select COUNT(*) from communities_tbl", callback);
+        return db.query("select * from communities_tbl", callback);
     },
-    userCount:function(callback)
-    {
-        return db.query("select COUNT(*) from user_tbl", callback);
+    userCount: function (callback) {
+        return db.query("select * from user_tbl", callback);
     }
 
 }
